ff2c9b6ebc5ba23f1561800a82f8a330912572fc
Kevin <kevhines@gmail.com>
2021-03-12 14:51:33 -0500

    commit tree c3da793c716c84a2e769bb9782e9914908cd3ac0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 } 
+    validates :summary, length: { maximum: 250 } 
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "Not a valid category" }
+    validate :title_not_clickbaity_enough
+
+    def title_not_clickbaity_enough
+        #binding.pry
+       if title
+            click_bait = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+            #binding.pry
+            result = click_bait.any? do |phrase|
+                self.title.include?(phrase) 
+            end
+            if !result
+                errors.add(:title, "not click-baity enough")
+            end
+        end
+    end
+
 end
